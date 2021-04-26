9a169271d8ed60cb6b48c276bf7a0f6c900235da
fhancquart <florent.hancquart@sarbacane.com>
2021-04-26 15:21:19 +0200

    Fix setup back

---
+++ src/utils/validateRegister.ts
@@ -0,0 +1,33 @@
+import {UsernamePasswordinput} from '../resolvers/UsernamePasswordInput'
+
+
+export const validateRegister = (options : UsernamePasswordinput) => {
+    if(!options.email.includes('@')){
+            return [{
+                    field: 'email',
+                    message: 'Email invalide'
+            }]            
+        }
+
+        if(options.username.length <= 2){
+            return[{
+                    field: 'username',
+                    message: 'Username pas assez long'
+                }]
+        }
+
+        if(options.username.includes('@')){
+            return[{
+                    field: 'username',
+                    message: 'Ne peut pas contenir d\'@'
+                }]
+        }
+
+        if(options.password.length <= 2){
+            return[{
+                    field: 'password',
+                    message: 'Mot de passe pas assez long'
+                }]
+        }
+
+        return null;
