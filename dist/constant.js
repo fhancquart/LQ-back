59238ac6df68826cec01c59dd86ea67e058f52ff
fhancquart <florent.hancquart@sarbacane.com>
2021-04-26 14:45:12 +0200

    change

--- dist/constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FROGET_PASSWORD_PREFIX = exports.COOKIE_NAME = exports._prod_ = void 0;
-exports._prod_ = process.env.NODE_ENV === 'prodution';
-exports.COOKIE_NAME = "qid";
-exports.FROGET_PASSWORD_PREFIX = "forget-password:";
-//# sourceMappingURL=constant.js.map
--- src/constant.ts
+++
@@ -1,3 +0,0 @@
-export const _prod_ = process.env.NODE_ENV === 'prodution';
-export const COOKIE_NAME = "qid";
-export const FROGET_PASSWORD_PREFIX = "forget-password:";
